88cabcaa1eb53df0aee6672ad331af3a5796b9ea
arshadrayyan50@gmail.com <arshadrayyan50@gmail.com>
2025-10-05 23:37:17 +0500

    connect with database

---
+++ CallerLog/Connection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallerLog
+{
+    internal class Connection
+    {
+        public SqlConnection con;
+        public SqlCommand cmd;
+        public SqlDataAdapter sda;
+        public string pkk;
+
+        public void connection()
+        {
+            con = new SqlConnection(@"Data Source=;Initial Catalog=CallManagement;Integrated Security=True");
+            con.Open();
+        }
+
+        public void dataSend(string SQl)
+        {
+            try
+            {
+                connection();
+                cmd = new SqlCommand(SQl, con);
+                cmd.ExecuteNonQuery();
+                pkk = "";
+            }
+            catch (Exception)
+            {
+                pkk = "Please check your Data";
+            }
+            con.Close();
+        }
+    }
+}
